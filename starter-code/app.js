04b9b850ff7f056abf6c4de0b757452b74afff2b
trumpetcoder <kevindjames55@gmail.com>
2017-12-21 09:14:43 -0700

    finished

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,17 +1,32 @@
 //Did you use npm install to
 //add all these packages
-//to our project?
+//to our project? (I sure DID)
 var express = require('express');
 var app = express();
+var PORT = process.env.PORT || 3000;
 var bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+
 // How do we 'require' the candyRouter file?
-var candyRouter;
+var candyRouter = require('./candyRouter');
 
 app.use(bodyParser.json());
 
 //How do we redirect the /candies path
 //through our candyRouter?
 //Hint: you need app.use
+app.use('/candies', candyRouter);
 
-app.listen(3000);
+
+	
+
+// app.listen(3000);
+
+app.listen(PORT, function() {
+	console.log("Hey I'm Working");
+});
+
+
+// req.body = candies.length + 1;
+// 	candies.push (req.body);
--- starter-code/candyRouter.js
+++ starter-code/candyRouter.js
@@ -1,17 +1,57 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-router = express.Router();
+var router = express.Router();
 
-var candies;
+var candies = [{"id":1,"name":"Chewing Gum","color":"Red"},
+			   {"id":2,"name":"Pez","color":"Green"},
+			   {"id":3,"name":"Marshmallow","color":"Pink"},
+			   {"id":4,"name":"Candy Stick","color":"Blue"}];
+
+
 
 //What would need to go into candies
 //in order to pass our first test?
 
-router.get('/', function(req,res) {
+router.get('/', function(req, res) {
 	// What would go here? 
-	// Hint: we want all candies in JSON format
+	// Hint: we want all candies in JSON format	
+	res.json(candies);
+	res.send('all the candies');
 });
 
 // Fill out the rest of the routes here
+
+router.get('/:id', function(req, res) {
+	// Show
+	// What would go here? 
+	// Hint: we want all candies in JSON format	
+	res.json(candies[req.params.id]);
+	res.send('candy according to id the candies');
+});
+
+// Check this in Postman
+router.post('/', function(req, res) {
+	// Post
+	req.body.id = candies.length + 1;
+	candies.push(req.body);	
+	res.send(req.body);
+	res.send('You POSTed a Candies');
+});
+
+// Check this in Postman
+router.put('/:id', function(req, res) {
+	// Put
+	res.send(candies[req.params.id] = (req.body));
+	res.send('You PUT "eidted" candy with an ID of ');
+});
+
+//  Check this in Postman
+router.delete('/:id', function(req, res) {
+	candies[req.params.id] = null;
+	res.send('You DELETEd the candy with the ID of ');
+	res.send(candies);
+});
+
+
 
 module.exports = router;
